cb9d62272674b0c960a01da32d546403ee7506d9
Mohammadmahdi Sefatzadeh <mm.80xefatzadeh@gmail.com>
2023-06-08 21:48:57 +0330

    [skip actions] metadataKeys type check

--- src/controllers/AttachmentTypeController.ts
+++ src/controllers/AttachmentTypeController.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Response } from 'express';
 import AttachmentType from '../models/AttachmentType';
 import Controller from '../utils/Controller';
+import { createError } from '../utils/errorFactory';
 import { IRequest } from '../utils/types';
 
 export default class AttachmentTypeController extends Controller<
@@ -25,7 +26,11 @@ export default class AttachmentTypeController extends Controller<
 	}
 
 	parseMetadataKeys(req: IRequest, res: Response, next: NextFunction) {
-		req.body.metadataKeys = (req.body.metadataKeys as string)?.split(',');
+		const keys = req.body.metadataKeys;
+		if (typeof keys === 'string')
+			req.body.metadataKeys = keys.split(',');
+		else if (!keys.length)
+			return next(createError(401, 'metadataKeys must be of type string or array'));
 
 		next();
 	}
