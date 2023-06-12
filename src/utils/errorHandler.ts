ac136dfbacba75211aaed383f99d12984ec2e533
Mohammadmahdi Sefatzadeh <mm.80xefatzadeh@gmail.com>
2023-06-12 23:46:11 +0330

    solved no name errors

--- src/utils/errorHandler.ts
+++ src/utils/errorHandler.ts
@@ -25,6 +25,7 @@ const handleValidationErrorDB = (err: any) => {
 };
 
 const modifyError = (err: any) => {
+	if (!err.name) err.name = err.message.split(':')[0];
 	/* database */
 	if (err.code === 11000) return handleDuplicateFieldsDB(err);
 	if (err.name === 'CastError') return handleCastErrorDB(err);
